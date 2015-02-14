0ddbb8e19cde3f7e1ef34b8fa568f53eada6490d
SamohvalovArt <admin@Admins-Mac-Pro-2.local>
2015-02-15 00:45:36 +0600

    Created new protocol HumanController

---
+++ Lab1/HumanController.h
@@ -0,0 +1,13 @@
+//
+//  HumanController.h
+//  Lab1
+//
+//  Created by Admin on 15/02/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#ifndef Lab1_HumanController_h
+#define Lab1_HumanController_h
+
+
+#endif
