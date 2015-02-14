210d1ba52f14d35b2fb9403533b64918f8de760a
SamohvalovArt <admin@Admins-Mac-Pro-2.local>
2015-02-14 23:03:34 +0600

    Created EducationMember protocol

---
+++ Lab1/EducationMember.h
@@ -0,0 +1,28 @@
+//
+//  EducationMember.h
+//  Lab1
+//
+//  Created by Admin on 14/02/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#ifndef Lab1_EducationMember_h
+#define Lab1_EducationMember_h
+
+#import "Student.h"
+#import "DeportamentDirector.h"
+#import "Teacher.h"
+#import "Deportament"
+
+@protocol EducationMember <NSObject>
+
+@optional
+-(void) setDeportament: (Deportament*) _deportament;
+-(void) setDeportamentDirector:(DeportamentDirector*) _depDirector;
+
+-(void) addStudent:(Student*) _student;
+-(void) addTeacher:(Teacher*) _teacher;
+
+@end
+
+#endif
